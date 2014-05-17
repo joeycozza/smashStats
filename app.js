3a76fe8d309920b3b0c4e51d70674eb1788d2d76
Joey Cozza <joeycozza@gmail.com>
2014-05-17 13:36:37 -0600

    getting routing set up for matches

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var matches = require('./routes/matches');
+var utils = require('./lib/utils');
 
 var app = express();
 
@@ -12,8 +14,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-
+app.use(utils.resolveCors);
 app.use('/', routes);
 app.use('/users', users);
+app.use('/matches', matches);
 
 module.exports = app;

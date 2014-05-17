3c22865b3ad5116fefb70d01f2bb76cf05e69f03
Joey Cozza <joeycozza@gmail.com>
2014-05-17 13:26:03 -0600

    hello world up and running

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-#!/usr/bin/env node
-var debug = require('debug')('generated-express-app');
-var app = require('../app');
+var app = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
